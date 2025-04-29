5cbdb6ce7d5b0fd7e769acf34055ecaad1e2afea
Chay <hitzcy@126.com>
2025-04-28 21:10:03 -0400

    personal setting

--- windows/switch_to_2.bat
+++ windows/switch_to_2.bat
@@ -1,5 +1,16 @@
-rem Switch MX Keys to channel 2
-.\hidapitester.exe --vidpid 046D:C52B --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x01,0x09,0x1e,0x01,0x00,0x00
+@echo off
 
-rem Switch MX Anywhere 3 to channel 2
-.\hidapitester.exe --vidpid 046D:C52B --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x02,0x0a,0x1b,0x01,0x00,0x00
+rem ========================
+rem Switch MX Keys S (Keyboard) to Channel 2
+rem ========================
+hidapitester.exe --vidpid 046D:B378 --usage 0x0202 --usagePage 0xFF43 --open --length 11 --send-output 0x11,0x00,0x0A,0x1E,0x01,0x00,0x00,0x00,0x00,0x00,0x00
+
+rem ========================
+rem Switch MX Master 3S (Mouse) to Channel 2
+rem ========================
+hidapitester.exe --vidpid 046D:B034 --usage 0x0202 --usagePage 0xFF43 --open --length 11 --send-output 0x11,0x00,0x0A,0x1E,0x01,0x00,0x00,0x00,0x00,0x00,0x00
+
+rem ========================
+rem (Optional) Switch MX Anywhere 3 to Channel 2
+rem ========================
+hidapitester.exe --vidpid 046D:C52B --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x02,0x0A,0x1B,0x01,0x00,0x00
